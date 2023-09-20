ba8b26ddedafd081b425170900db09c6563d547f
Rimeshsapkota <rimesh@lancemeup.com>
2023-09-20 16:04:12 +0545

    setup new project

---
+++ backend/yatra-griha/src/main/java/com/rk/yatragriha/YatraGrihaApplication.java
@@ -0,0 +1,13 @@
+package com.rk.yatragriha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatraGrihaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatraGrihaApplication.class, args);
+	}
+
+}
---
+++ backend/yatra-griha/src/test/java/com/rk/yatragriha/YatraGrihaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.yatragriha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraGrihaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
